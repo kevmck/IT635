3424be68b71d22797b41e7eac0dc46ba31f36314
kevmck <klm25@njit.edu>
2017-02-25 23:50:34 -0500

    Renter and apartment insertion code works as expected; pyLib functions work correctly.

--- apts.py
+++ apts.py
@@ -6,8 +6,27 @@ db = MySQLdb.connect(host = "localhost", user = "root", passwd = "Clloyd20", db 
 
 dbCur = db.cursor()
 
-#yLib.clearScreen(os)
+#pyLib.clearScreen(os)
 #pyLib.descApt(dbCur)
-#pyLib.aptInsert(dbCur, db)
+
+"""
+#Inserting new apartment listing
+rent = str(raw_input("Enter rent: "))
+bed = str(raw_input("Enter number of bedrooms: "))
+bath = str(raw_input("Enter number of bathrooms: "))
+style = '\'' + str(raw_input("Enter style - choose from contemporary, cottage, mediterranean, traditional, rustic or retro: ")) + '\''
+age = str(raw_input("Enter age of apartment: "))
+loc = '\'' + str(raw_input("Enter location: ")) + '\''
+status = '\'' + str(raw_input("Enter the rental status: ")) + '\''
+pyLib.aptInsert(dbCur, db, rent, bed, bath, style, age, loc, status)
+"""
+
+#Inserting new renter
+userName = '\'' + str(raw_input("Enter username: ")) + '\''
+locSearch = '\'' + str(raw_input("Enter location of interest: ")) + '\''
+stySearch = '\'' + str(raw_input("Enter desired style: ")) + '\''
+minRent = str(raw_input("Enter minimum rent range: "))
+maxRent = str(raw_input("Enter maximum rent range: "))
+pyLib.renterInsert(dbCur, db, userName, locSearch, stySearch, minRent, maxRent)
 
 db.close
--- pyLib.py
+++ pyLib.py
@@ -10,8 +10,14 @@ def descRenter(cursor):
     for row in cursor.fetchall():
         print (row)
 
-def aptInsert(cursor, dataBase):
-	cursor.execute("INSERT INTO apt VALUES (1, 2203, 2, 2, 'condominium', 80, 'Hackensack', 1)")
+def aptInsert(cursor, dataBase, r, be, ba, sty, aa, lo, sta):
+	cursor.execute("INSERT INTO apt (aptnumber,rent,bedrooms,bathrooms,style,aptage,location,rentalstatus) \
+					VALUES (NULL," + r + "," + be + "," + ba + "," + sty + "," + aa + "," + lo + "," + sta + ")")
+	dataBase.commit()
+
+def renterInsert(cursor, dataBase, uname, loS, styS, minR, maxR):
+	cursor.execute("INSERT INTO renter (renteruname,locationsearch,stylesearch,minrent,maxrent) \
+					VALUES (" + uname + "," + loS + "," + styS + "," + minR + "," + maxR + ")")
 	dataBase.commit()
 
 def clearScreen(os):
