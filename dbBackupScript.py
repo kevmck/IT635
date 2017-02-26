c7309c19ff37b0ea8df900e2dbabacb34f494106
kevmck <klm25@njit.edu>
2017-02-26 18:05:46 -0500

    Created database backup script; completed first backup successfully.

---
+++ dbBackupScript.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import datetime, os
+
+os.system("mysqldump -u root -p apartments > /home/kev/it635/sqlbackup/" + str((datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S'))) + ".sql")
+
+print("Database backup complete!\n\nPress any key to continue...")
+
+raw_input()
