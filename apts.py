04aa390420f4651bf39e0dc2c4391d79bd584000
kevmck <klm25@njit.edu>
2017-03-02 23:47:03 -0500

    Updated procedures to provide more user feedback; added filtering for apartment style input.

--- apts.py
+++ apts.py
@@ -29,11 +29,18 @@ while selection:
 		rent = str(raw_input("Enter rent: "))
 		bed = str(raw_input("Enter number of bedrooms: "))
 		bath = str(raw_input("Enter number of bathrooms: "))
+		
 		style = '\'' + str(raw_input("Enter style - choose from contemporary, cottage, mediterranean, traditional, rustic or retro: ")) + '\''
+		while style not in ("contemporary", "cottage", "mediterranean", "traditional", "rustic", "retro"):
+			style = raw_input("Please enter a valid style: ")
+		else:
+			style = '\'' + style + '\''	
+		
 		age = str(raw_input("Enter age of apartment: "))
 		loc = '\'' + str(raw_input("Enter location: ")) + '\''
 		status = '\'' + str(raw_input("Enter the rental status: ")) + '\''
 		pyLib.aptInsert(dbCur, db, rent, bed, bath, style, age, loc, status)
+		print ("Successfully created new apartment!")
 
 	elif selection == '2':
 		#Inserting new renter
@@ -44,6 +51,7 @@ while selection:
 		minRent = str(raw_input("Enter minimum rent range: "))
 		maxRent = str(raw_input("Enter maximum rent range: "))
 		pyLib.renterInsert(dbCur, db, userName, locSearch, stySearch, minRent, maxRent)
+		print ("Successfully created profile for " + userName + "!")
 
 	elif selection == '3':
 		print ("Apartment search for renter\n")
@@ -52,9 +60,9 @@ while selection:
 		#Updating rental status of an apartment
 		print ("Update status of rental\n")
 		aptNum = raw_input("Enter the apartment number to update: ")
-		currStat = raw_input("Enter the current status of the apartment: ")
+		currStat = raw_input("Enter the current status of the apartment - (r)ented or (v)acant: ")
 		pyLib.aptStatUpdate(dbCur, db, aptNum, currStat)			
-		print("Update successful!")
+		print("Successfully updated status for apartment #" + aptNum + "; set to (" + currStat + ").")
 
 	elif selection == '5':
 		print ("Generate rental report\n")
