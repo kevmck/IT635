d3dc2b368d0e875e9e22c53ec921dd79ac36c79d
kevmck <klm25@njit.edu>
2017-04-07 09:01:13 -0400

    Updated DB backup script to support Windows file naming scheme.

--- dbBackupScript.py
+++ dbBackupScript.py
@@ -2,7 +2,7 @@
 
 import datetime, os
 
-os.system("mysqldump -u root -p apartments > /home/kev/it635/sqlbackup/" + str((datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S'))) + ".sql")
+os.system("mysqldump -u root -p apartments > /home/kev/it635/sqlbackup/" + str((datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))) + ".sql")
 
 print("Database backup complete!\n\nPress any key to continue...")
 
