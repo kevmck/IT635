40143197b8b5b3d5e29b6b0daeed1141a61017b6
kevmck <klm25@njit.edu>
2017-04-20 08:56:53 -0400

    Added ability to insert data. mLab receives test data and adds documents correctly.

--- mongoLib.py
+++ mongoLib.py
@@ -5,6 +5,7 @@ print("Testing mLab connection...\n")
 r = os.system("ping -q -c 1 ds155820.mlab.com")
 
 #Test connection to mLab before attempting to execute queries.
+#This test can be removed before the final, or if it causes unexpected results.
 if (r == 0):
 	print("\nConnection to mLab successful!\n")
 	client = MongoClient('mongodb://admin:password@ds155820.mlab.com:55820/it635')
@@ -14,10 +15,16 @@ else:
 
 
 db = client.it635
-collection = db.test
+collection = db.IT635_notes
 
-#Show names of collections
+#Show names of collections. Comment/remove before final.
 print(db.collection_names(include_system_collections=False))
+
+#Insert test data
+userName = raw_input("Enter user's name: ")
+userNote = raw_input("Enter user's notes: ")
+insertNote = {"username": userName, "note": userNote}
+insertProc = collection.insert_one(insertNote).inserted_id
 
 #Fetch single item from database
 pprint.pprint(collection.find_one())
